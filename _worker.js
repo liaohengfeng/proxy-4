ab4dee63d6a55398630b9ed796c726b1ab986743
liaohengfeng <156047906+liaohengfeng@users.noreply.github.com>
2024-06-21 21:08:42 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,12 @@
+export default 
+{
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = 'panel4.serv00.com'
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
