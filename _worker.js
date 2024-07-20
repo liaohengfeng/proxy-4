11eaef95e3101b2fb9362dbb8cab7db5d207d2f1
liaohengfeng <156047906+liaohengfeng@users.noreply.github.com>
2024-07-20 20:11:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ export default
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'panel4.serv00.com'
+      url.hostname = 'www.bzhai.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
